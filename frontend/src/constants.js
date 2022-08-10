df39e7618ed9693b601d531d4dfb4d2ed22d08d8
Ankit Shubham <ankitshubham97@gmail.com>
2022-08-11 01:21:58 +0530

    fix: correct the api url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-// export const API_BASE_URL = "https://authfa2-backend.herokuapp.com";
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://authfa2-backend.herokuapp.com";
+// export const API_BASE_URL = "http://localhost:3000";
 export const STRING_NONCE = "msg";
 export const PACKED_NONCE = "05013132366437333637";
